e479bb53562c9bc0c8ffc75d6f2e62a4e12d97e3
Jin G <116611815+srcJin@users.noreply.github.com>
2023-07-17 17:58:50 +0800

    tab component without fancy styles

--- src/components/Tab/index.js
+++ src/components/Tab/index.js
@@ -3,8 +3,6 @@ import { PureComponent } from "react";
 
 import "./index.scss";
 
-import Tab from '../Tab'
-
 // the data structure of tabList
 // tabList = [
 //   {
@@ -14,20 +12,64 @@ import Tab from '../Tab'
 // ];
 
 export default class Tab extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentId: 0,
+    };
+  }
+
+  componentDidMount() {
+    const { initTab, tabList } = this.props;
+    if (initTab == undefined) {
+      this.setState({
+        currentId: tabList?.[0]?.["id"]
+      });
+    } else {
+      this.setState({
+        currentId: initTab,
+      });
+    }
+  }
+
+  // create a function to change the state of selected item
+  handleClick = (id) => {
+    this.setState({
+      currentId: id,
+    });
+    this.props.onTabClick?.(id);
+  };
+
+  handleChange = (e) => {
+    const id = e.detail.current;
+    this.setState(
+      {
+        currentId: id,
+      },
+      () => {
+        // 将当前id传递到外部
+        this.props.onChange?.(e);
+      }
+    );
+  };
+
   render() {
     const { className, tabList } = this.props;
+    const { currentId } = this.state;
+    
     return (
       <View className={`tab-container ${className}`}>
         <View className="tab-bar">
           {/* ? in here tests whether tabList is null, if not null, continue execute  */}
           {tabList?.map((item) => {
             return (
-              <View className="tab-item" key={item.id}>
+              <View className={`tab-item ${currentId === item.id ? 'active' : ''}`} key={item.id} onClick={() => this.handleClick(item.id)}>
                 {item.label}
               </View>
             );
           })}
         </View>
+        
       </View>
     );
   }
--- src/pages/flight/index/index.js
+++ src/pages/flight/index/index.js
@@ -11,31 +11,38 @@ import Taro from "@tarojs/taro";
 
 import "./index.scss";
 
-import Tab from '../Tab'
+import Tab from "../../../components/Tab";
 
 const FLIGHT_TABS = [
-    {
-        label: '单程',
-        id: 0,
-    },
-    {
-        label: '往返',
-        id: 1,
-    },
-    {
-        label: '多程',
-        id: 2,
-    },
-]
-
+  {
+    label: "单程",
+    id: 0,
+  },
+  {
+    label: "往返",
+    id: 1,
+  },
+  {
+    label: "多程",
+    id: 2,
+  },
+];
 
 export default class FlightIndex extends PureComponent {
+  handleTabClick = (id) => {
+    console.log(id);
+  };
   render() {
-    return <View className="flight-container">
+    return (
+      <View className="flight-container">
         <View className="flight-top">
-        <Tab tablist={FLIGHT_TABS}/>
-            </View>
-
-    </View>;
+          <Tab
+            tabList={FLIGHT_TABS}
+            onTabClick={this.handleTabClick}
+            className="flight-index-tab"
+          ></Tab>
+        </View>
+      </View>
+    );
   }
 }
