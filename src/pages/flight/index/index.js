ffce1bb283e3493a6650105ef8718697bfb82019
Jin G <116611815+srcJin@users.noreply.github.com>
2023-07-21 09:22:31 +0800

    successfully called imageAPI

--- src/pages/flight/index/index.js
+++ src/pages/flight/index/index.js
@@ -13,6 +13,9 @@ import "./index.scss";
 
 import Tab from "../../../components/Tab";
 
+//import image api
+import {imageReq } from '@/common/api'
+
 const FLIGHT_TABS = [
   {
     label: "单程",
@@ -29,9 +32,24 @@ const FLIGHT_TABS = [
 ];
 
 export default class FlightIndex extends PureComponent {
+
+  // react 生命周期, 在mount时调用getImages请求
+  componentDidMount() {
+    this.getImages()
+  }
+
+  // add a method to fetch the API
+  getImages = () => {
+    imageReq().then((res) => {
+      console.log(res);
+    })
+  }
+
   handleTabClick = (id) => {
     console.log(id);
   };
+
+
   render() {
     return (
       <View className="flight-container">
